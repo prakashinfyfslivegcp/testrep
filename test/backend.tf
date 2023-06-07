5c8ec68d44b00c6414d7440f7d4e196e8a19e928
Prakash Chakkaravarthy <prakash.c01@infosys.com>
2023-06-07 11:43:10 +0530

    deleted test folder

--- test/backend.tf
+++
@@ -1,6 +0,0 @@
-terraform {
-  backend "gcs" {
-    bucket = "cloud-build-deployments-tfstate-gcp"
-    prefix = "env/stg"
-  }
-}
--- test/version.tf
+++
@@ -1,8 +0,0 @@
-provider "google" {
-  project = var.project
-  region  = var.region
-}
-
-terraform {
-  required_version = ">=1.2.2"
-}
